5788c6f870008e09dea3ad8d7206a2d586df12d2
Arina Avlasova <avlasva@mail.ru>
2025-09-05 16:04:07 +0300

    feat: add playwright automation tests for sauce demo

---
+++ tests/login.spec.js
@@ -0,0 +1,18 @@
+import {test, expect} from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
+test ('check the authorization on Sauce Demo', async ({page}) =>{
+    await page.goto('https://www.saucedemo.com/');
+    await expect(page.getByPlaceholder('Username')).toBeVisible();
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await expect(page.getByPlaceholder('Password')).toBeVisible();
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.locator('.submit-button.btn_action').click();
+    await expect (page).toHaveURL('https://www.saucedemo.com/inventory.html');
+});
+test ('unsuccessful authorization on Sauce Demo', async({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.locator('.submit-button.btn_action').click();
+    await expect (page.locator('[data-test = "error"]')).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+})
